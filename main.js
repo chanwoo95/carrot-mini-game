865c1c578d1f622a31688f1c6b27579446cdd867
chanwoo95 <cksdndbd95@gmail.com>
2021-04-09 21:48:13 +0900

    javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+const gameBtn = document.querySelector('.game__button');
+const gameScore = document.querySelector('.game__score');
+const gameTimer = document.querySelector('.game__timer');
+const gameField = document.querySelector('.game__field');
+
+
+gameBtn.addEventListener('click', () => {
+    console.log('click!')
+})
+
