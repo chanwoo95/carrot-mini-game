b23727c4b7653b8a35f135baa7aea4f51c20d4e3
chanwoo95 <cksdndbd95@gmail.com>
2021-04-10 21:32:09 +0900

    start game

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 const gameBtn = document.querySelector('.game__button');
+const playBtn = document.querySelector('.fa-play');
 const gameScore = document.querySelector('.game__score');
 const gameTimer = document.querySelector('.game__timer');
 const gameField = document.querySelector('.game__field');
 
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
 
-gameBtn.addEventListener('click', () => {
-    console.log('click!')
-})
 
+function showStopBtn() {
+    playBtn.classList.remove('fa-play');
+    playBtn.classList.add('fa-stop');
+}
+
+function showStartBtn() {
+    playBtn.classList.remove('fa-stop');
+    playBtn.classList.add('fa-play');
+    
+}
+
+
+
+function startGame() {
+    gameBtn.addEventListener("click", () => {
+        showStopBtn();
+    });
+}
+
+function paintField(path, count) {
+    
+}
+
+function init() {
+    startGame();
+    paintField('img/carrot.png', CARROT_COUNT);
+    paintField('img/bug.png', BUG_COUNT);
+}
+
+init();
