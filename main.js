a6d28e236eb91679ab788f8d064528d2bd09a7f3
chanwoo95 <cksdndbd95@gmail.com>
2021-04-11 23:04:22 +0900

    add random item

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const playBtn = document.querySelector('.fa-play');
 const gameScore = document.querySelector('.game__score');
 const gameTimer = document.querySelector('.game__timer');
 const gameField = document.querySelector('.game__field');
+const fieldRect = gameField.getBoundingClientRect();
 
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
@@ -27,14 +28,29 @@ function startGame() {
     });
 }
 
-function paintField(path, count) {
+function addItem(className, imgPath, count) {
+   
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width;
+    const y2 = fieldRect.height; 
     
+    for(let i=0; i<count; i++){
+        const item = document.createElement('img');
+        item.setAttribute('className', className);
+        item.setAttribute('src', imgPath);
+        item.style.position='absolute';
+
+        item.randomNum(x1,y1);
+        item.randomNum(x2,y2);
+        gameField.appendChild(item);
+    }
 }
 
 function init() {
     startGame();
-    paintField('img/carrot.png', CARROT_COUNT);
-    paintField('img/bug.png', BUG_COUNT);
+    addItem('carrot', 'img/carrot.png', CARROT_COUNT);
+    addItem('bug', 'img/bug.png', BUG_COUNT);
 }
 
 init();
